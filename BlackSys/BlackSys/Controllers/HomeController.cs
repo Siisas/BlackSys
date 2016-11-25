7b0946896b2962934aa546dc65a4b56a6dc45891
Ricardo <Ricardo@RIVIRTUAL>
2016-11-24 21:27:44 -0500

    primer siis proyecto

---
+++ BlackSys/BlackSys/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using BlackSys.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace BlackSys.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private BlackSysEntities db = new BlackSysEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult LoadMenu()
+        {
+            var menu = from a in db.MenuTemp
+                               select a;
+            return View(menu.ToList());
+        }
+
+
+    }
+}
